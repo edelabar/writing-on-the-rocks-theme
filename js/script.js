22653fd68cae1f0e666fb8316db1158f00208099
edelabar <edelabar@gmail.com>
2011-05-18 19:30:30 -0700

    a few more tweaks...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 /* Author: Eric DeLabar */
 
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console) {
+      arguments.callee = arguments.callee.caller;
+      console.log( Array.prototype.slice.call(arguments) );
+  }
+};
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
+
 /* http://adactio.com/journal/4470/ */
 if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
   var viewportmeta = document.querySelector('meta[name="viewport"]');
